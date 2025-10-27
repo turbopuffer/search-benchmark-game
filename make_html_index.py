1bb5d268bc6df969ba1cd221572852036280fee8
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-10-26 23:35:00 -0400

    build HTML index

---
+++ make_html_index.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+build_dir = Path("build")
+results = [d for d in build_dir.iterdir() if d.is_dir()]
+results.sort(reverse=True)
+
+html = "<html>\n<head>\n<title>turbopuffer Search Benchmark Results</title>\n</head>\n<body face=monospace>\n<h1>turbopuffer Search Benchmark Results</h1>\n<ul>\n"
+
+for result in results:
+    result_name = result.name
+    html += f'  <li><a href="{result_name}/">{result_name}</a></li>\n'
+
+html += "</ul>\n</body>\n</html>"
+
+(build_dir / "index.html").write_text(html)
