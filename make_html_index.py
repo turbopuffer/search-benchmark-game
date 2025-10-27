30a904d2ef65c78d411c84ceb178c15b75efd16b
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-10-26 23:43:05 -0400

    fix monospace font

--- make_html_index.py
+++ make_html_index.py
@@ -5,7 +5,8 @@ build_dir = Path("build")
 results = [d for d in build_dir.iterdir() if d.is_dir()]
 results.sort(reverse=True)
 
-html = "<html>\n<head>\n<title>turbopuffer Search Benchmark Results</title>\n</head>\n<body face=monospace>\n<h1>turbopuffer Search Benchmark Results</h1>\n<ul>\n"
+html = "<html>\n<head>\n<title>turbopuffer Search Benchmark Results</title>\n</head>\n"
+html += '<body style="font-family: monospace">\n<h1>turbopuffer Search Benchmark Results</h1>\n<ul>\n'
 
 for result in results:
     result_name = result.name
