90480d08f8c3430d7a7d196651029566f2f120f7
Adrien Grand <jpountz@gmail.com>
2025-11-19 17:01:46 +0100

    Add Python script to compare multiple bench runs.

---
+++ diff_results.py
@@ -0,0 +1,64 @@
+import numpy as np
+from scipy import stats
+
+TPUF = "turbopuffer"
+GREEN = '\033[92m'
+RED = '\033[91m'
+COLOR_END = '\033[0m'
+
+def print_diff(baseline, contender):
+  first = True
+  for collection_type in baseline:
+    if collection_type not in contender:
+      continue
+    if first:
+      first = False
+    else:
+      print("")
+    print(f"\033[1m{collection_type}\033[0m")
+    print("                           Query     Baseline avg  Baseline stddev    Contender avg Contender stddev            Change          p-value")    
+    print_diff_engine(baseline[collection_type][TPUF], contender[collection_type][TPUF])
+
+def print_diff_engine(baseline, contender):
+  baseline_query_to_latencies = {}
+  for query in baseline:
+    baseline_query_to_latencies[query["query"]] = query["duration"]
+
+  for query in contender:
+    query_str = query["query"]
+    if query_str not in baseline_query_to_latencies:
+      print("[WARN] Query [%s] not found in baseline results\n" %query_str)
+
+    baseline_durations = np.array(baseline_query_to_latencies[query_str])
+    contender_durations = np.array(query["duration"])
+
+    if len(query_str) > 32:
+      query_str = query_str[:31] + "…"
+
+    baseline_mean = np.mean(baseline_durations)
+    baseline_stddev = np.std(baseline_durations, ddof=1)
+    contender_mean = np.mean(contender_durations)
+    contender_stddev = np.std(contender_durations, ddof=1)
+    change = 100 * (contender_mean - baseline_mean) / baseline_mean
+    p_value = stats.ttest_ind(baseline_durations, contender_durations, equal_var=True)[1]
+    print(f"{query_str:>32} {baseline_mean:16.2f} {baseline_stddev:16.2f} {contender_mean:16.2f} {contender_stddev:16.2f} {GREEN if change <= 0 else RED}{change:16.2f}%{COLOR_END} {p_value:16.2f}")
+
+    #print("%s % 5.2f % 5.2f % 5.2f % 5.2f % 5.2f\n" %('{0: >32}'.format(query_str),
+    #    np.mean(baseline_durations),
+    #    np.std(baseline_durations, ddof=1),
+    #    np.mean(contender_durations),
+    #    np.std(contender_durations, ddof=1),
+    #    stats.ttest_ind(baseline_durations, contender_durations, equal_var=True)[1]))
+
+
+if __name__ == '__main__':
+  import json, sys
+
+  if len(sys.argv) != 3:
+    print("Usage: %s results1.json results2.json" %sys.argv[0])
+    sys.exit(1)
+
+  with open(sys.argv[1]) as baseline_file, open(sys.argv[2]) as contender_file:
+    baseline = json.load(baseline_file)
+    contender = json.load(contender_file)
+    print_diff(baseline["results"], contender["results"])
