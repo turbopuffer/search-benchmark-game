ea2d81d3bd516bd52ef463d48a46f8f8820718ff
Adrien Grand <jpountz@gmail.com>
2025-11-21 19:22:35 +0100

    Make diffs more readable.

--- diff_results.py
+++ diff_results.py
@@ -16,7 +16,7 @@ def print_diff(baseline, contender):
     else:
       print("")
     print(f"\033[1m{collection_type}\033[0m")
-    print("                           Query     Baseline avg  Baseline stddev    Contender avg Contender stddev            Change          p-value")    
+    print("                                            Query \ Latency (μs)     Baseline avg  Baseline stddev    Contender avg Contender stddev            Change          p-value")    
     print_diff_engine(baseline[collection_type][TPUF], contender[collection_type][TPUF])
 
 def print_diff_engine(baseline, contender):
@@ -32,8 +32,8 @@ def print_diff_engine(baseline, contender):
     baseline_durations = np.array(baseline_query_to_latencies[query_str])
     contender_durations = np.array(query["duration"])
 
-    if len(query_str) > 32:
-      query_str = query_str[:31] + "…"
+    if len(query_str) > 64:
+      query_str = query_str[:63] + "…"
 
     baseline_mean = np.mean(baseline_durations)
     baseline_stddev = np.std(baseline_durations, ddof=1)
@@ -41,7 +41,7 @@ def print_diff_engine(baseline, contender):
     contender_stddev = np.std(contender_durations, ddof=1)
     change = 100 * (contender_mean - baseline_mean) / baseline_mean
     p_value = stats.ttest_ind(baseline_durations, contender_durations, equal_var=True)[1]
-    print(f"{query_str:>32} {baseline_mean:16.2f} {baseline_stddev:16.2f} {contender_mean:16.2f} {contender_stddev:16.2f} {GREEN if change <= 0 else RED}{change:16.2f}%{COLOR_END} {p_value:16.2f}")
+    print(f"{query_str:>64} {baseline_mean:16.2f} {baseline_stddev:16.2f} {contender_mean:16.2f} {contender_stddev:16.2f} {GREEN if change <= 0 else RED}{change:16.2f}%{COLOR_END} {p_value:16.2f}")
 
 
 if __name__ == '__main__':
